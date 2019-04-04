3f505e19ed2aac7162cb978d8937a381eebe63c2
vvhardy0143 <vvhardy0143@H202R1S3.STUDENT.ad.fgcu.edu>
2019-04-04 16:02:14 -0400

    Added Exceptions

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 // Vladimir Hardy
@@ -8,36 +9,60 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Choose an option below by typing a number to begin");
-    System.out.println("1. Introduction");
-    System.out.println("2. Hangman");
-    System.out.println("3. Calculator");
-    System.out.println("4. Car");
-
-    int menu = 0;
-    Scanner input = new Scanner(System.in);
-    menu = input.nextInt();
-    switch (menu) {
-      case 1:
-
-        Introduction.intro();
-        Introduction.normalMethod();
-        MethodOverride.normalMethod();
-        break;
-      case 2:
-        Hangman.hangman();
-        break;
-      case 3:
-        Calculator.calculateMain();
-        break;
-      case 4:
-        Car.car();
-        SUV mySUV = new SUV();
-        mySUV.pimpedOutSUV();
-        break;
-      default:
-        System.out.println("Invalid selection, please type a number");
-    }
-    input.close();
+	Boolean tryAgain = true;
+	Scanner input = new Scanner(System.in);
+	while (tryAgain) {
+	    System.out.println("Choose an option below by typing a number to begin");
+	    System.out.println("1. Introduction");
+	    System.out.println("2. Hangman");
+	    System.out.println("3. Calculator");
+	    System.out.println("4. Car");
+	
+	    int menu = 0;
+	    
+	    try {
+	    menu = input.nextInt();
+	    }
+	    catch (InputMismatchException ex) { //Specific exception handling
+	    	System.out.println("Please enter a whole number");
+	    	input.nextLine(); //Gets rid of garbage input
+	    }
+	    catch(Exception ex) { //More general exception
+	    	System.out.println("Please enter a whole number");
+	    	input.nextLine();
+	    }
+	    switch (menu) {
+	      case 1:
+	
+	        Introduction.intro();
+	        Introduction.normalMethod();
+	        MethodOverride.normalMethod();
+	        break;
+	      case 2:
+	        Hangman.hangman();
+	        break;
+	      case 3:
+	        Calculator.calculateMain();
+	        break;
+	      case 4:
+	        Car.car();
+	        SUV mySUV = new SUV();
+	        mySUV.pimpedOutSUV();
+	        break;
+	      default:
+	        System.out.println("Invalid selection, please type a number");
+	    }
+	    System.out.println("Would you like to try again? (yes or no)");
+	    String restart = input.nextLine();
+	    if (restart.equals("yes")) {
+	    	tryAgain = true;
+	    	input.reset();
+	    }
+	    else {
+	    	System.out.println("Have a nice day!");
+	    	tryAgain = false;
+	    }
+	}
+	input.close();
   }
 }
